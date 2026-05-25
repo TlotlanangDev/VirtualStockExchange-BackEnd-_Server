d7a8926c0a0a19009bc2b24e34f0b1ffb0ec0343
Tlotlanang <tlotlananggabonewe@gmail.com>
2026-05-25 14:30:49 +0200

    initial commit

---
+++ src/main/java/com/tlotlanang/virtualstockexchangebackend/VirtualStockExchangeBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.tlotlanang.virtualstockexchangebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualStockExchangeBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualStockExchangeBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tlotlanang/virtualstockexchangebackend/VirtualStockExchangeBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlotlanang.virtualstockexchangebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualStockExchangeBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
