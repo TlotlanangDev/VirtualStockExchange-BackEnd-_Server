d218ce734b28084a4b88158b0cd82c2006010eab
Tlotlanang <tlotlananggabonewe@gmail.com>
2026-07-06 13:54:42 +0200

    added more assertion to UserRegisterRepositoryTest

---
+++ src/test/java/com/tlotlanang/virtualstockexchangebackend/userRegister/repository/UserRegisterRepositoryTest.java
@@ -0,0 +1,54 @@
+package com.tlotlanang.virtualstockexchangebackend.userRegister.repository;
+
+
+import com.tlotlanang.virtualstockexchangebackend.userRegister.entity.UserRegisterEntity;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+
+import java.time.LocalDate;
+
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+class UserRegisterRepositoryTest {
+
+    @Autowired
+    private UserRegisterRepository userregisterrepository;
+
+
+    @Test
+    public void setUserRegisterRepository_Save_ReturnSavedUser(){
+
+        UserRegisterEntity userRegisterEntity= UserRegisterEntity.builder()
+                .uuid(null)
+                .name("Tlotlanang")
+                .surName("Gabonewe")
+                .dateOfBirth(LocalDate.of(2002,9,1))
+                .phoneNumber("0787060708")
+                .emailAddress("Tlotlanang@gmail.com")
+                .passWord("ergdg43gr").build();
+
+        UserRegisterEntity savedToEntity = userregisterrepository.save(userRegisterEntity);
+
+        Assertions.assertThat(savedToEntity).isNotNull();
+        Assertions.assertThat(savedToEntity.getUuid()).isNotNull();
+        Assertions.assertThat(savedToEntity.getName()).isEqualTo("Tlotlanang").isNotNull();
+        Assertions.assertThat(savedToEntity.getSurName()).isEqualTo("Gabonewe").isNotNull();
+        Assertions.assertThat(savedToEntity.getDateOfBirth()).isEqualTo(LocalDate.of(2002,9,1)).isNotNull();
+        Assertions.assertThat(savedToEntity.getPhoneNumber()).isEqualTo("0787060708").isNotNull();
+        Assertions.assertThat(savedToEntity.getEmailAddress()).isEqualTo("Tlotlanang@gmail.com").isNotNull();
+        Assertions.assertThat(savedToEntity.getPassWord()).isEqualTo("ergdg43gr").isNotNull();
+
+
+
+    }
+
+
+}
+
