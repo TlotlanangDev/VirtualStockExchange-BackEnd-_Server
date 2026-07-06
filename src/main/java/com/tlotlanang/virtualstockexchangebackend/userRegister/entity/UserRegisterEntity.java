beab3ca0f224595883b1b5d020ec592ecae07736
Tlotlanang <tlotlananggabonewe@gmail.com>
2026-07-06 18:15:21 +0200

    added validation annotations for UserRegisterEntity class and created a test class.

--- src/main/java/com/tlotlanang/virtualstockexchangebackend/userRegister/entity/UserRegisterEntity.java
+++ src/main/java/com/tlotlanang/virtualstockexchangebackend/userRegister/entity/UserRegisterEntity.java
@@ -1,6 +1,7 @@
 package com.tlotlanang.virtualstockexchangebackend.userRegister.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
 import java.time.LocalDate;
@@ -17,11 +18,24 @@ public class UserRegisterEntity {
         @GeneratedValue
         @UuidGenerator
         private UUID uuid;
+        @NotBlank(message = "Please Enter your Name.")
+        @Size(min = 2, max = 50, message = "name must be between 2 & 50 characters.")
         private String name;
+        @NotBlank(message = "Please Enter your surName.")
+        @Size(min = 2, max = 50, message = "surname must be between 2 & 50 characters.")
         private String surName;
+        @NotNull(message = "Please Enter Date of Birth.")
+        @Past(message = "Enter Correct Date of Birth.")
         private LocalDate dateOfBirth;
+        @Pattern(regexp = "^(\\+27|0)[1-9][0-9\\s\\-]{8,}$",
+                message = "Please Enter Correct Phone Number")
         private String phoneNumber;
+        @Size(min = 5, max = 50, message = "Email Address must be between 5 & 50 characters.")
+        @Email(message = "Please Enter valid Email Address.")
+        @NotBlank(message = "Please Enter Email address.")
         private String emailAddress;
+        @Size(min = 8, max = 40, message = "Passowrd should have at least 8 characters.")
+        @NotBlank(message = "passWord cannot be Empty..")
         private String passWord;
 
 }
---
+++ src/test/java/com/tlotlanang/virtualstockexchangebackend/userRegister/entity/UserRegisterEntityTest.java
@@ -0,0 +1,77 @@
+package com.tlotlanang.virtualstockexchangebackend.userRegister.entity;
+
+import com.tlotlanang.virtualstockexchangebackend.userRegister.domain.UserRegisterDto;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRegisterEntityTest {
+
+    private Validator validation;
+    @BeforeEach
+    void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validation = factory.getValidator();
+    }
+
+    @Test
+    public void setUseRegisterEntity_passValidInfo_returnDataPassed(){
+        UserRegisterEntity userRegisterEntity = UserRegisterEntity.builder()
+                .uuid(null)
+                .name("Tlotlanang")
+                .surName("Gabonewe")
+                .dateOfBirth(LocalDate.of(2002,9,1))
+                .phoneNumber("0787058697")
+                .emailAddress("Tlotlanang@gmail.com")
+                .passWord("ergdg43gr").build();
+
+        Set<ConstraintViolation<UserRegisterEntity>> violations = validation.validate(userRegisterEntity);
+
+        List<String> failedProperties = violations.stream()
+                .map(violation -> violation.getPropertyPath().toString())
+                .toList();
+
+        Assertions.assertThat(violations).isEmpty();
+        Assertions.assertThat(violations.size()).isEqualTo(0);
+
+    }
+
+    @Test
+    public void setUserRegisterEntity_passInValidInfo_returnData(){
+        UserRegisterEntity userRegisterEntity = UserRegisterEntity.builder()
+
+                .name("d")
+                .surName("s")
+                .dateOfBirth(LocalDate.of(2032,9,1))
+                .phoneNumber("fd0787058697")
+                .emailAddress("Tlotlananggmailcom")
+                .passWord("").build();
+
+        Set<ConstraintViolation<UserRegisterEntity>> violations = validation.validate(userRegisterEntity);
+
+        List<String> failedProperties = violations.stream()
+                .map(violation -> violation.getPropertyPath().toString())
+                .toList();
+
+        Assertions.assertThat(violations).isNotEmpty();
+        Assertions.assertThat(failedProperties).contains("name",
+                "surName",
+                "dateOfBirth",
+                "phoneNumber",
+                "emailAddress",
+                "passWord");
+
+
+    }
+
+}
