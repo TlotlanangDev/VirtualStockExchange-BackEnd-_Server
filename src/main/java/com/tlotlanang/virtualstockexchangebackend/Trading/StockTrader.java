8d2de172817aa9611f62f07a9e22d603348efb06
Tlotlanang <tlotlananggabonewe@gmail.com>
2026-07-13 18:52:06 +0200

    start creating trading innterface for buying and selling of stock.

--- src/main/java/com/tlotlanang/virtualstockexchangebackend/Trading/StockTrader.java
+++ src/main/java/com/tlotlanang/virtualstockexchangebackend/Trading/StockTrader.java
@@ -4,9 +4,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-public interface StockTrader<TradeResponseDto, TradeRequestDto> {
-    @GetMapping("/trade")
-    ResponseEntity<TradeResponseDto> buyStock(@RequestBody TradeRequestDto requestBody);
-    ResponseEntity<TradeResponseDto> sellStock(@RequestBody TradeRequestDto requestBody);
+public interface StockTrader<StockTradeResponseDto, StockTradeRequestDto> {
+    @GetMapping("/buyStock")
+    ResponseEntity<StockTradeResponseDto>buyStock(@RequestBody StockTradeRequestDto requestBody);
+
+    @GetMapping("/sellStock")
+    ResponseEntity<StockTradeResponseDto>sellStock(@RequestBody StockTradeRequestDto requestBody);
 
 }
