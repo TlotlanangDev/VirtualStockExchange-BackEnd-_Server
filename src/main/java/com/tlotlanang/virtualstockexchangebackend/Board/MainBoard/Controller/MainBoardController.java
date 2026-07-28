dc81b7127327cc0ef20ccb852d2682065b0a1296
Tlotlanang <tlotlananggabonewe@gmail.com>
2026-07-28 18:51:58 +0200

    added links to the controller and implemented new method

--- src/main/java/com/tlotlanang/virtualstockexchangebackend/Board/MainBoard/Controller/MainBoardController.java
+++ src/main/java/com/tlotlanang/virtualstockexchangebackend/Board/MainBoard/Controller/MainBoardController.java
@@ -11,13 +11,16 @@ import org.springframework.data.web.SlicedResourcesAssembler;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.SlicedModel;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 
 @RestController
-@RequestMapping("api/v1/stockExchange/mainboard")
+@RequestMapping("api/v1/stockExchange/mainboard/listings")
 public class MainBoardController implements Board<MainBoardResponseDto> {
 
     @Autowired
@@ -29,7 +32,7 @@ public class MainBoardController implements Board<MainBoardResponseDto> {
     @Autowired
     private SlicedResourcesAssembler<MainBoardEntity> slicedAssembler;
 
-
+    @GetMapping
     @Override
     public ResponseEntity<SlicedModel<EntityModel<MainBoardResponseDto>>> listing(Pageable pageable) {
 
@@ -40,5 +43,19 @@ public class MainBoardController implements Board<MainBoardResponseDto> {
                 );
 
         return ResponseEntity.ok(mainBoardSlicedModel);
+    }
+
+    @GetMapping
+    @Override
+    public ResponseEntity<EntityModel<MainBoardResponseDto>> getByEmail(String emailAddress) {
+
+        var entity = mainBoardService.viewByEmail(emailAddress);
+
+        MainBoardResponseDto mainBoardResponseDto = mainBoardMapper.toDto(entity);
+
+        var mainListedCompanyselfLink = linkTo(methodOn(MainBoardController.class).getByEmail(emailAddress)).withSelfRel();
+
+        EntityModel<MainBoardResponseDto>mainResponseEntityModel = EntityModel.of(mainBoardResponseDto, mainListedCompanyselfLink);
+        return ResponseEntity.ok(mainResponseEntityModel);
     }
 }
