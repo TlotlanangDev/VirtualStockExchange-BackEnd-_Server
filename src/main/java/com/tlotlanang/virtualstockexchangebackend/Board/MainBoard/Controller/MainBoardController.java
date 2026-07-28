b4d2199bcc702a44af69423c0afee11d3cad5e5b
Tlotlanang <tlotlananggabonewe@gmail.com>
2026-07-28 18:10:25 +0200

    main board controller creation

--- src/main/java/com/tlotlanang/virtualstockexchangebackend/Board/MainBoard/Controller/MainBoardController.java
+++ src/main/java/com/tlotlanang/virtualstockexchangebackend/Board/MainBoard/Controller/MainBoardController.java
@@ -1,24 +1,44 @@
 package com.tlotlanang.virtualstockexchangebackend.Board.MainBoard.Controller;
 
 import com.tlotlanang.virtualstockexchangebackend.Board.Board;
-import com.tlotlanang.virtualstockexchangebackend.Board.MainBoard.domain.MainBoardRequestDto;
+import com.tlotlanang.virtualstockexchangebackend.Board.MainBoard.Entity.MainBoardEntity;
 import com.tlotlanang.virtualstockexchangebackend.Board.MainBoard.domain.MainBoardResponseDto;
+import com.tlotlanang.virtualstockexchangebackend.Board.MainBoard.mapper.MainBoardMapper;
+import com.tlotlanang.virtualstockexchangebackend.Board.MainBoard.service.MainBoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
-import org.springframework.hateoas.CollectionModel;
+import org.springframework.data.web.SlicedResourcesAssembler;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.SlicedModel;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collection;
-import java.util.List;
 
-//@RestController
+
+@RestController
+@RequestMapping("api/v1/stockExchange/mainboard")
 public class MainBoardController implements Board<MainBoardResponseDto> {
+
+    @Autowired
+    private MainBoardService mainBoardService;
+
+    @Autowired
+    private MainBoardMapper mainBoardMapper;
+
+    @Autowired
+    private SlicedResourcesAssembler<MainBoardEntity> slicedAssembler;
 
 
     @Override
-    public ResponseEntity<SlicedModel<EntityModel<MainBoardResponseDto>>> temporarylisting(Pageable pageable) {
-        return null;
+    public ResponseEntity<SlicedModel<EntityModel<MainBoardResponseDto>>> listing(Pageable pageable) {
+
+        var mainBoardEntitySlice = mainBoardService.getListings(pageable);
+
+        SlicedModel<EntityModel<MainBoardResponseDto>>mainBoardSlicedModel = slicedAssembler.
+                toModel(mainBoardEntitySlice, entity -> EntityModel.of(mainBoardMapper.toDto(entity))
+                );
+
+        return ResponseEntity.ok(mainBoardSlicedModel);
     }
 }
