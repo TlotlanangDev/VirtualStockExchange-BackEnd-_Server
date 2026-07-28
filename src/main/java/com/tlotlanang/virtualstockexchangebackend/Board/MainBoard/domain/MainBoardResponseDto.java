45034659b00d429d7d28643d1c118c073bef4fd1
Tlotlanang <tlotlananggabonewe@gmail.com>
2026-07-28 18:12:06 +0200

    main board Mapper creation

--- src/main/java/com/tlotlanang/virtualstockexchangebackend/Board/MainBoard/domain/MainBoardResponseDto.java
+++ src/main/java/com/tlotlanang/virtualstockexchangebackend/Board/MainBoard/domain/MainBoardResponseDto.java
@@ -1,8 +1,16 @@
 package com.tlotlanang.virtualstockexchangebackend.Board.MainBoard.domain;
 
 
+import lombok.Builder;
 
-public class MainBoardResponseDto {
+import java.math.BigDecimal;
 
+@Builder
+public record MainBoardResponseDto(
 
-}
+        Integer Id,
+        String companyName,
+        String registrationNumber,
+        Integer stockShare,
+        BigDecimal pricePerShare
+) {}
