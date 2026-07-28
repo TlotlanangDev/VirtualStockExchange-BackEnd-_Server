df4e7ac8170033c5f4c13449effa2216ef87b456
Tlotlanang <tlotlananggabonewe@gmail.com>
2026-07-28 18:50:00 +0200

    Added new endpoint for board interface

--- src/main/java/com/tlotlanang/virtualstockexchangebackend/Board/Board.java
+++ src/main/java/com/tlotlanang/virtualstockexchangebackend/Board/Board.java
@@ -6,9 +6,13 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.SlicedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 public interface Board <BoardResponseDto>{
 
-    @GetMapping("/listings")
+    @GetMapping
     ResponseEntity<SlicedModel<EntityModel<BoardResponseDto>>> listing(@Valid Pageable pageable );
+
+    @GetMapping("/{emailAddress}")
+    ResponseEntity<EntityModel<BoardResponseDto>>getByEmail(@PathVariable("emailAddress") String emailAddress);
 }
