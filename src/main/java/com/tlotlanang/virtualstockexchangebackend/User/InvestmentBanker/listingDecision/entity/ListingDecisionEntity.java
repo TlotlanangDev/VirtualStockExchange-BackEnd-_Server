38d4a288a68d3a2b5d37c3d3d007f081624c4395
Tlotlanang <tlotlananggabonewe@gmail.com>
2026-08-03 17:57:51 +0200

    changed target table name.

--- src/main/java/com/tlotlanang/virtualstockexchangebackend/User/InvestmentBanker/listingDecision/entity/ListingDecisionEntity.java
+++ src/main/java/com/tlotlanang/virtualstockexchangebackend/User/InvestmentBanker/listingDecision/entity/ListingDecisionEntity.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 
 @Entity
-@Table(name = "MainListingAcc")
+@Table(name = "TemporaryListingAcc")
 @Data
 @Builder
 @NoArgsConstructor
