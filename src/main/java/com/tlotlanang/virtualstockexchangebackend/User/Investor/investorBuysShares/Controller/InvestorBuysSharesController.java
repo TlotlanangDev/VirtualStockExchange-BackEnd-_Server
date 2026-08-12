db8de16c02732bd8acfc309273719cfe3f82815f
Tlotlanang <tlotlananggabonewe@gmail.com>
2026-08-12 15:01:02 +0200

    Investor buys shares controller added

--- src/main/java/com/tlotlanang/virtualstockexchangebackend/User/Investor/investorBuysShares/Controller/InvestorBuysSharesController.java
+++ src/main/java/com/tlotlanang/virtualstockexchangebackend/User/Investor/investorBuysShares/Controller/InvestorBuysSharesController.java
@@ -2,8 +2,9 @@ package com.tlotlanang.virtualstockexchangebackend.User.Investor.investorBuysSha
 
 import com.tlotlanang.virtualstockexchangebackend.User.StockTrader;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@RequestMapping
 public class InvestorBuysSharesController implements StockTrader {
     @Override
     public ResponseEntity buyStock(Object requestBody) {
