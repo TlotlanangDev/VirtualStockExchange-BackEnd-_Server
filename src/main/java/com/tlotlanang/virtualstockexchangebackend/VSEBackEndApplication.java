cd5c99d9add356764b7580795b44449e2d88714e
Tlotlanang <tlotlananggabonewe@gmail.com>
2026-09-21 15:11:13 +0200

    Banker Login Entity test class with methods

--- src/main/java/com/tlotlanang/virtualstockexchangebackend/VSEBackEndApplication.java
+++ src/main/java/com/tlotlanang/virtualstockexchangebackend/VSEBackEndApplication.java
@@ -14,4 +14,6 @@ public class VSEBackEndApplication {
         SpringApplication.run(VSEBackEndApplication.class, args);
     }
 
+
+
 }
--- src/test/java/com/tlotlanang/virtualstockexchangebackend/User/InvestmentBanker/loginBanker/entity/BankerLoginEntityTest.java
+++ src/test/java/com/tlotlanang/virtualstockexchangebackend/User/InvestmentBanker/loginBanker/entity/BankerLoginEntityTest.java
@@ -1,7 +1,54 @@
 package com.tlotlanang.virtualstockexchangebackend.User.InvestmentBanker.loginBanker.entity;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Set;
 
 class BankerLoginEntityTest {
+
+    private Validator validation;
+    @BeforeEach
+    void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validation = factory.getValidator();
+    }
+
+    @Test
+    public void setBankerLoginEntity_passVaslidData_returnDataPassed(){
+        BankerLoginEntity bankerLoginEntity = BankerLoginEntity.builder()
+                .emailAddress("tlotlananggabonewe@gmail.com")
+                .passWord("f7esfuf2fuwi").build();
+
+        Set<ConstraintViolation<BankerLoginEntity>>violations = validation.validate(bankerLoginEntity);
+
+        Assertions.assertThat(violations).isEmpty();
+        Assertions.assertThat(violations.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void setBankerLoginEntity_passInvalidData_returnDataPassed(){
+        BankerLoginEntity bankerLoginEntity = BankerLoginEntity.builder()
+                .emailAddress("tlotlananggabonewegmailcom")
+                .passWord("i").build();
+
+        Set<ConstraintViolation<BankerLoginEntity>>violations = validation.validate(bankerLoginEntity);
+
+        List<String>failedProperties = violations.stream()
+                .map(violation->violation.getPropertyPath().toString()).toList();
+
+        Assertions.assertThat(violations).isNotEmpty();
+        Assertions.assertThat(failedProperties).contains(
+                "emailAddress", "passWord");
+
+
+    }
 
 }
